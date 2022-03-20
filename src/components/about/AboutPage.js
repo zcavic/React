80b2965854764485b5c21110deb779c1db66e5ac
Zeljko <zeljko.cavic@playstudios.com>
2022-03-20 20:14:04 +0100

    Added Header

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const AboutPage = () => {
+const AboutPage = () => (
     <div>
         <h2>About</h2>
         <p>This app uses React, Redux, React Router and many other helpful libraries.</p>
-    </div>;
-};
+    </div>
+);
 
 export default AboutPage;
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const HomePage = () => {
+const HomePage = () => (
     <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
         <p>React, Redux and React Router for ultra-responsive web apps.</p>
         <Link to="about" className="btn btn-primary btn-lg">
             Learn more
         </Link>
-    </div>;
-};
+    </div>
+);
 
 export default HomePage;
